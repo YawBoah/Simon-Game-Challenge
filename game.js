c3675d773104dfbffe06994239297c5c199caa6c
YBoah <yawboahene.yb@gmail.com>
2023-11-01 11:10:56 +0000

    Adding Sounds to Button Clicks

--- game.js
+++ game.js
@@ -2,30 +2,33 @@
 var buttonColours =["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 $(".btn").click(function() {
 
     
     var userChosenColour = $(this).attr("id");
-  
-    
     userClickedPattern.push(userChosenColour);
   
     //console.log(userClickedPattern);
+    playSound(userChosenColour);
   
   });
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*3)+1;
+    var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
 
     $("#" + randomChosenColour).fadeIn(50).fadeOut(100).fadeIn(50);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-
+    playSound(randomChosenColour);
 }
+
+function playSound(name) {
+
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+  
