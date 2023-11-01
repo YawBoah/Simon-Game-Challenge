97e780066dd69e5653870528c72dea77eb6980e1
YBoah <yawboahene.yb@gmail.com>
2023-11-01 11:57:18 +0000

    Checking the User's Answer Against the Game Sequence

--- game.js
+++ game.js
@@ -18,19 +18,43 @@ $(document).keypress(function() {
   });
 
 $(".btn").click(function() {
-
-    
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
   
     //console.log(userClickedPattern);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
   
   });
 
-function nextSequence() {
-    level++;
+function checkAnswer(currentLevel) {
 
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+    }
+
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
     $("#level-title").text("Level " + level);
 
 
