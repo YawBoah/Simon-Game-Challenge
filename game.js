8251a29bb8d710d82516445c503a4a2f8ea6b2fc
YBoah <yawboahene.yb@gmail.com>
2023-11-01 05:20:45 +0000

    Added the sounds

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
-var gamePattern = [];
-var randomChosenColour = ;
 
 var buttonColours =["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random()*3)+1;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+
+    $("#" + randomChosenColour).fadeIn(50).fadeOut(100).fadeIn(50);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
 }
