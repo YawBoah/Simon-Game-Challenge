433db437f6e11ab0547aa2452bba02573f7989c1
YBoah <yawboahene.yb@gmail.com>
2023-11-01 05:11:00 +0000

    Js additions

---
+++ game.js
@@ -0,0 +1,8 @@
+var gamePattern = [];
+var randomChosenColour = ;
+
+var buttonColours =["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*3)+1;
+}
